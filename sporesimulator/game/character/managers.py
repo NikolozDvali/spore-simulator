2f90b16cc0e95d5757f607a43c00a254fce7be18
ndval21 <ndval21@freeuni.edu.ge>
2024-10-18 16:48:37 +0400

    refactor

--- sporesimulator/game/character/managers.py
+++ sporesimulator/game/character/managers.py
@@ -6,7 +6,6 @@ class PositionManager:
     def __init__(self, position: int = 0):
         self.position = position
 
-
 class AppendageManager:
     def __init__(self,
                  legs: Legs | None = None,
@@ -18,6 +17,44 @@ class AppendageManager:
         self.claws = claws if claws is not None else Claws()
         self.teeth = teeth if teeth is not None else Teeth()
 
+    """Common attribute getters"""
+
+    @property
+    def wing_count(self):
+        return self.wings.count()
+
+    @property
+    def leg_count(self):
+        return self.legs.count()
+
+    @property
+    def teeth_level(self):
+        return self.teeth.level
+
+    @property
+    def claw_level(self):
+        return self.claws.level
+
+    """Common attribute setters"""
+
+    @leg_count.setter
+    def leg_count(self, count: int):
+        self.legs.count = count
+
+    @wing_count.setter
+    def wing_count(self, count: int):
+        self.wings.count = count
+
+    @teeth_level.setter
+    def teeth_level(self, level: int):
+        self.teeth.level = level
+
+    @claw_level.setter
+    def claw_level(self, level: int):
+        self.claws.level = level
+
+    """Common methods"""
+
     def calculate_attack_power(self, base_attack_power: int) -> int:
         attack_power = base_attack_power
         if self.claws:
@@ -25,38 +62,6 @@ class AppendageManager:
         if self.teeth:
             attack_power = self.teeth.modify_attacking_power(attack_power)
         return attack_power
-
-    @property
-    def wing_count(self):
-        return self.wings.count()
-
-    @wing_count.setter
-    def wing_count(self, count: int):
-        self.wings.count = count
-
-    @property
-    def leg_count(self):
-        return self.legs.count()
-
-    @leg_count.setter
-    def leg_count(self, count: int):
-        self.legs.count = count
-
-    @property
-    def teeth_level(self):
-        return self.teeth.level
-
-    @teeth_level.setter
-    def teeth_level(self, level: int):
-        self.teeth.level = level
-
-    @property
-    def claw_level(self):
-        return self.claws.level
-
-    @claw_level.setter
-    def claw_level(self, level: int):
-        self.claws.level = level
 
 class CharacterStatsManager:
     def __init__(self,
@@ -66,8 +71,3 @@ class CharacterStatsManager:
         self.health = health
         self.stamina = stamina
         self.attacking_power = attacking_power
-
-    def decrease_stamina_by(self, decrease_amount: int):
-        if decrease_amount > self.stamina:
-            raise ValueError('decrease_amount must be less than stamina')
-        self.stamina -= decrease_amount
