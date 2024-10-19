9c9077677182808a1e910d21858a6e53287cb186
ndval21 <ndval21@freeuni.edu.ge>
2024-10-19 13:46:56 +0400

    added movement agent tests

---
+++ sporesimulator/tests/movement/test_greedy_movement_agent.py
@@ -0,0 +1,14 @@
+from sporesimulator.game.movement.move import GreedyMovementAgent, Crawl, Hop, Walk, Run, Fly
+
+
+def test_greedy_movement_agent_no_possible_moves():
+    agent = GreedyMovementAgent()
+    assert agent.next_move([]) is None
+
+def test_greedy_movement_agent_some_possible_moves():
+    agent = GreedyMovementAgent()
+    assert agent.next_move([Crawl, Hop]) == Hop
+
+def test_greedy_movement_agent_all_possible_moves():
+    agent = GreedyMovementAgent()
+    assert agent.next_move([Crawl, Hop, Walk, Run, Fly]) == Fly
